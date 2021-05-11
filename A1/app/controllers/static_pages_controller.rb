8e431d1eccece33d69f8c08cd619edb9b35fc46d
s3844286 <s3844286@student.rmit.edu.au>
2021-05-11 10:57:10 +0000

    Added Images to View + basic layout changes

--- A1/app/controllers/static_pages_controller.rb
+++ A1/app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    maximumItems = Item.count
+    randomItemIndex = 1 + rand(maximumItems)
+    @item = Item.find(randomItemIndex)
   end
 end
--- A1/db/schema.rb
+++ A1/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210510112557) do
+ActiveRecord::Schema.define(version: 20210511090507) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "username"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20210510112557) do
     t.string   "company"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image_url"
   end
 
 end
