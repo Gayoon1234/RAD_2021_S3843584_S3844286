f078d0c539a6214a80e89f934c14e301880fc8b3
Gayoon1234 <s3843584@student.rmit.edu.au>
2021-05-16 03:28:17 +0000

    fixed duplication bug

--- A1/app/controllers/saves_controller.rb
+++ A1/app/controllers/saves_controller.rb
@@ -1,21 +1,30 @@
 class SavesController < ApplicationController
   
   def show
+    
+    # we get the current featured item
     @item = $featuredItem
-    newSave = Save.create(name: @item.name, username:nil)
-    newSave.save!;
     
-    @SavedList = Array.new
     
-    Save.all.each do |saveItem|
-      Item.where(name: saveItem.name).each{|item| @SavedList.push(item)}
+    #add it to the database, for username:nil to work I commented out a line in the save.rb model
+        if Save.where(:name => @item.name).blank?
+      newSave = Save.create(name: @item.name, username: nil)
+      newSave.save!
     end
     
     
+    # for each item saved  by the current user
+    @SavedList = Array.new
+    Save.where(username: nil).each do |saveItem|
+      Item.where(name: saveItem.name).each{|item| @SavedList.push(item) if !@SavedList.include?(item)}
+                                                                        #this if statement filters out duplicate entries already present
+    end
+    
+    # this just appends some test stuff to the end
     @arr = Array.new
     @arr.push("test")
     @arr.push(Save.count)
-    
+
   end
   
 end
