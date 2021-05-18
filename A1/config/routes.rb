f9aef3227bd3e9411409bce95a57b3c6070057ed
s3844286 <s3844286@student.rmit.edu.au>
2021-05-18 05:43:03 +0000

    Sign in / out

--- A1/config/routes.rb
+++ A1/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   
+  devise_for :users
+  
+  # devise_for :users do
+  # get '/users/sign_out', to: 'devise/sessions#destroy'
+  # end
+  
   resources :saves
   get '/home', to: 'static_pages#home'
 
--- A1/db/migrate/20210510112255_add_index_to_customers_email.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexToCustomersEmail < ActiveRecord::Migration[5.0]
- def change
-  add_index :customers, :email, unique: true
- end
-end
-
