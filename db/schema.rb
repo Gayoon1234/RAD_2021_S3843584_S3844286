2b288b06c6046ff845de537b2cc383f9fefc33c0
s3844286 <s3844286@student.rmit.edu.au>
2021-05-19 05:53:54 +0000

    Pushing to Git and Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -24,13 +24,6 @@ ActiveRecord::Schema.define(version: 20210518031354) do
     t.string   "image_url"
   end
 
-  create_table "saves", force: :cascade do |t|
-    t.string   "name"
-    t.string   "username"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
