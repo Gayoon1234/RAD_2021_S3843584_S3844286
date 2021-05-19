bff4dee49e6a0e20ec41555394e950bd2a287a13
s3844286 <s3844286@student.rmit.edu.au>
2021-05-19 01:37:39 +0000

    postgres

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,9 @@
 class StaticPagesController < ApplicationController
   
   def home
-    # maximumItems = Item.count
-    # randomItemIndex = 1 + rand(maximumItems)
-    # @featuredItem = Item.find(randomItemIndex)
+    maximumItems = Item.count
+    randomItemIndex = 1 + rand(maximumItems)
+    @featuredItem = Item.find(randomItemIndex)
     @itemCountWomens = Item.where(section:"Womens").count
     @itemCountMens = Item.where(section:"Mens").count
     @itemCountKids = Item.where(section:"Childs").count
