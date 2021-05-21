914e28441415b50adc1a3893b2d2e0b7282a1281
s3844286 <s3844286@student.rmit.edu.au>
2021-05-21 14:07:21 +0000

    removed migrations causing migration error for Cart table

--- db/migrate/20210521134339_rename_size_in_cart.rb
+++
@@ -1,7 +0,0 @@
-class RenameSizeInCart < ActiveRecord::Migration[5.2]
-  def change
-    
-    rename_column :carts, :size, :clothing_size
-    
-  end
-end
--- db/migrate/20210521135906_remove_clothing_size.rb
+++
@@ -1,7 +0,0 @@
-class RemoveClothingSize < ActiveRecord::Migration[5.2]
-  def change
-    
-    remove_column :carts, :clothing_size
-    
-  end
-end
